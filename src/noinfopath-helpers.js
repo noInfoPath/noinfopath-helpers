429f1cf286230428e8d6e05842132c07432a811b
russ <russ@imginconline.com>
2015-03-19 16:33:31 -0400

    corrected toObject function, attributes object now using attribute names,the object is also flattened

--- src/noinfopath-helpers.js
+++ src/noinfopath-helpers.js
@@ -1,5 +1,5 @@
 /**
- * #noinfopath-helpers@0.0.12
+ * #noinfopath-helpers@0.0.13
  * NoInfoPath Helpers
  */
 
@@ -190,7 +190,10 @@
 					if(node.hasAttributes()){
 						angular.forEach(node.attributes, function(val, name){
 							//console.log(name, val.textContent);
-							obj.attributes[name] = val.textContent;
+							//Using attributes for backwards compatibility for the time being
+							obj.attributes[val.name] = val.textContent;
+
+							obj[val.name] = val.textContent;
 						});
 					}
 

52078e36058949df8e37a4abee6735eabcf3a5b6
Jeff <jeff@imginconline.com>
2016-06-22 12:48:49 -0400

    Added configureWatches to noActionQueue.

--- src/no-action-queue.js
+++ src/no-action-queue.js
@@ -1,30 +1,197 @@
 (function(angular, undefined) {
 	angular.module('noinfopath.helpers')
 
-	.service("noActionQueue", ["$q", function($q) {
-		function _recurse(deferred, results, execQueue, i){
-			var action = execQueue[i];
-			if(action){
-				action()
-				.then(function(deferred, results, execQueue, i, data){
-					results[i] = data;
-					_recurse(deferred, results, execQueue, ++i);
-				}.bind(null, deferred, results, execQueue, i))
-				.catch(deferred.reject);
-			}else {
-				deferred.resolve(results);
+		.service("noActionQueue", ["$injector", "$q", function($injector, $q) {
+			function _recurse(deferred, results, execQueue, i){
+				var action = execQueue[i];
+				if(action){
+					action()
+					.then(function(deferred, results, execQueue, i, data){
+						results[i] = data;
+						console.log("execAction finished", i, data);
+						_recurse(deferred, results, execQueue, ++i);
+					}.bind(null, deferred, results, execQueue, i))
+					.catch(deferred.reject);
+				}else {
+					deferred.resolve(results);
+				}
 			}
-		}
 
-		function _synchronize(execQueue) {
-			var deferred = $q.defer();
-			var results = [];
-			_recurse(deferred, results, execQueue, 0);
-			return deferred.promise;
-		}
+			function _resolveActionProvider(action){
+				try{
+					return $injector.get(action.provider);
+				}catch(err){
+					console.error("invalid action provider: ", action);
+					return;
+				}
+			}
 
-		this.synchronize =  _synchronize;
-	}])
+			function _resolveActionParams(scope, params){
+				var promises = [];
+
+				for(var p=0; p<params.length; p++){
+					var param = params[p];
+
+					if(angular.isObject(param)){
+						if(param.provider === "scope"){
+							promises.push($q.when(noInfoPath.getItem(scope, param.property)));
+						}else{
+							var prov = _resolveActionProvider(param),
+								method = prov ? prov[param.method] : undefined,
+								property = param.property ? noInfoPath.getItem(prov, param.property) : undefined;
+
+							if(method){
+								promises.push($q.when(method()));
+							}else if(property){
+								promises.push($q.when(property));
+							}else{
+								promises.push($q.reject({"error": "Invalid parameter", data: param}));
+							}
+						}
+					}else{
+						promises.push($q.when(param));
+					}
+
+				}
+
+				return $q.all(promises);
+			}
+
+			function _noop(action, params){
+				console.error("Could not resolve method from action config.", action, params);
+				return $q.when({method: angular.noop, params: params});
+			}
+
+			function _resolveActionMethodAndParams(scope, action){
+				var prov = _resolveActionProvider(action),
+					method = prov ? prov[action.method] : _noop.bind(null, action);
+
+				return _resolveActionParams(scope, action.params || [])
+					.then(function(params){
+						return {method: method ||  _noop.bind(null, action), params: params};
+					})
+					.catch(function(err){
+						console.error(err);
+					});
+
+			}
+
+			function _createActionExecFunction(ctx, scope, el, action){
+				function actionExecFunction(ctx, scope, el, action){
+					return $q(function(resolve, reject){
+						_resolveActionMethodAndParams(scope, action)
+							.then(function(result){
+								var returnValue = result.method.apply(ctx, action.noContextParams ? result.params : [ctx, scope, el].concat(result.params));
+
+								if(returnValue && returnValue.then){
+									returnValue
+										.then(resolve)
+										.catch(reject);
+								}else{
+									resolve(returnValue);
+								}
+
+							})
+							.catch(reject);
+
+					});
+
+				}
+
+				return actionExecFunction.bind(ctx, ctx, scope, el, action);
+
+			}
+
+			function _createWatchFunction(ctx, scope, el, action){
+				function actionExecFunction(ctx, scope, el, action, newval, oldval, effectiveScope){
+					return $q(function(resolve, reject){
+						if(newval !== oldval){
+							_resolveActionMethodAndParams(scope, action)
+								.then(function(result){
+									var returnValue = result.method.apply(ctx, action.noContextParams ? result.params : [ctx, scope, el].concat(result.params));
+
+									if(returnValue && returnValue.then){
+										returnValue
+											.then(resolve)
+											.catch(reject);
+									}else{
+										resolve(returnValue);
+									}
+
+								})
+								.catch(reject);
+
+						}else{
+							resolve(ctx);
+						}
+
+					});
+
+				}
+
+				return actionExecFunction.bind(ctx, ctx, scope, el, action);
+
+			}
+
+			/*
+				* 	```json
+				* 	[{
+				* 	 "scope": "current|parent|root"
+				* 	 "scopeKey": "projectTabs",
+				* 	 "action": {
+				* 		 "provider": "noNavigationManager",
+				* 		 "method": "changeNavBar",
+				* 		 "params": [
+				* 			 {
+				* 				 "provider": "scope|scope.$parent|scope.$root",
+				* 				 "property": "projectTabs.btnBar"
+				* 			 }
+				* 		 ]
+				* 	 }
+				* 	}]
+				* 	```
+			*/
+			function _configureWatch(ctx, scope, el, watch) {
+				var execQueue =  _createActionQueue(ctx, scope, el, watch.actions),
+					watchFn = _synchronize.bind(ctx, execQueue);
+
+				scope.$watch(watch.scopeKey, watchFn);
+			}
+
+			function _configureWatches(ctx, scope, el, watches){
+				for(var w=0; w<watches.length; w++){
+					var watch = watches[w];
+
+					_configureWatch(ctx, scope, el, watch);
+				}
+			}
+			this.configureWatches = _configureWatches;
+
+
+			function _createActionQueue(ctx, scope, el, actions, fnWrapper){
+				var execFns = [];
+
+				for(var a=0; a<actions.length; a++){
+					var action = actions[a],
+						execFn = fnWrapper ? fnWrapper(ctx, scope, el, action) : _createActionExecFunction(ctx, scope, el, action);
+
+					execFns[a] = execFn;
+				}
+
+				return execFns;
+			}
+			this.createQueue = _createActionQueue;
+
+			function _synchronize(execQueue) {
+				var deferred = $q.defer();
+				var results = [];
+				_recurse(deferred, results, execQueue, 0);
+				return deferred.promise;
+			}
+			this.synchronize =  _synchronize;
+
+
+		}])
 	;
 
 })(angular);
